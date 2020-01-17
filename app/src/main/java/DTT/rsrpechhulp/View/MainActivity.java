50c217a183220566772006eec0e3ba609f820ef2
TiberiuBucur <tbucur7000@yahoo.com>
2020-01-17 01:26:16 +0000

    Buttons implementation in first activity done

--- app/src/main/java/DTT/rsrpechhulp/View/MainActivity.java
+++ app/src/main/java/DTT/rsrpechhulp/View/MainActivity.java
@@ -2,19 +2,50 @@ package DTT.rsrpechhulp.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import DTT.rsrpechhulp.R;
 
 public class MainActivity extends AppCompatActivity implements MainMenu {
+
+    private TextView textView;
+    private Button closeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = (TextView) findViewById(R.id.infoTV);
+        String title = getResources().getString(R.string.title);
+        String message = getResources().getString(R.string.message);
+        textView.setText(Html.fromHtml("<b>" + title + "</b><br />" + message));
+
+        Button mapBtn = (Button) findViewById(R.id.MapButton);
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        closeBtn = (Button) findViewById(R.id.CloseButton);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setVisibility(View.GONE);
+                closeBtn.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
@@ -23,4 +54,23 @@ public class MainActivity extends AppCompatActivity implements MainMenu {
         menuInflater.inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.infoButton){
+            textView.setVisibility(View.VISIBLE);
+            closeBtn.setVisibility(View.VISIBLE);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void loadPhone(){
+
+    }
+
+    public void loadTablet(){
+
+    }
+
 }
--- app/src/main/java/DTT/rsrpechhulp/View/MainMenu.java
+++ app/src/main/java/DTT/rsrpechhulp/View/MainMenu.java
@@ -2,5 +2,6 @@
 package DTT.rsrpechhulp.View;
 
 public interface MainMenu {
-
+    void loadPhone();
+    void loadTablet();
 }
