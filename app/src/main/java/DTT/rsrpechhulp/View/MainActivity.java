c3739cea423e6a498af0bf599f35ccca3e6bd2b1
TiberiuBucur <tbucur7000@yahoo.com>
2020-01-21 23:08:34 +0000

    Google map working and some minor adjustments to the UI

--- app/src/main/java/DTT/rsrpechhulp/View/MainActivity.java
+++ app/src/main/java/DTT/rsrpechhulp/View/MainActivity.java
@@ -1,7 +1,6 @@
 package DTT.rsrpechhulp.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
--- app/src/main/java/DTT/rsrpechhulp/View/MapActivity.java
+++ app/src/main/java/DTT/rsrpechhulp/View/MapActivity.java
@@ -1,17 +1,110 @@
 package DTT.rsrpechhulp.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import DTT.rsrpechhulp.R;
 
 public class MapActivity extends AppCompatActivity implements UI {
 
+    private GoogleMap map;
+    private MapView mapView;
+
+    private static final String[] PERMS={
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    private static final int REQUEST = 1337;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        Button backBtn = (Button) findViewById(R.id.back_button);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                if(!hasPermission(PERMS[0]) || !hasPermission(PERMS[1])){
+                    requestPermissions(PERMS, REQUEST);
+                }
+                map = googleMap;
+                map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                map.setMyLocationEnabled(true);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        mapView.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        mapView.onLowMemory();
+        super.onLowMemory();
+    }
+
+    @Override
+    protected void onStart() {
+        mapView.onStart();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        super.onStop();
+    }
+
+    private boolean hasPermission(String perm){
+        return PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm);
     }
 
     public void loadPhone() {
